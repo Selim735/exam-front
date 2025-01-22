0ab87f220c9be0294f36a2305872cb4444a49f07
selim <s.benslimen2706@myu.imset.tn>
2025-01-22 09:19:41 +0100

    install comosant roombooking

---
+++ src/app/room-booking/room-booking.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-booking',
+  standalone: false,
+  
+  templateUrl: './room-booking.component.html',
+  styleUrl: './room-booking.component.css'
+})
+export class RoomBookingComponent {
+
+}
